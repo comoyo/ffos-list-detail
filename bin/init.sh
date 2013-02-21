a9785dc4cc00eb2dd5a63948b48ba51857893e01
Kuba Kucharski <kuba.kucharski@gmail.com>
2013-02-21 11:35:38 +0100

    Update bin/init.sh
    3 first lines cause problems and does not make sense. When you install using previous version you will get -> 
    cd: www/: No such file or directory. This is a fix. 

--- bin/init.sh
+++ bin/init.sh
@@ -1,6 +1,6 @@
-CMD="$(readlink $0)"
-CMD_DIR=`dirname "$CMD"`
-cd "$CMD_DIR/.."
+#CMD="$(readlink $0)"
+#CMD_DIR=`dirname "$CMD"`
+#cd "$CMD_DIR/.."
 
 npm install bower -g
 git submodule update --init --recursive
