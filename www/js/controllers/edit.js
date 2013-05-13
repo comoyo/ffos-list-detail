507e11700cb2fc7e7e970484388297c1a3b5b9fa
Jan Jongboom <janjongboom@gmail.com>
2013-05-13 04:20:09 -0400

    Edit screen should use a cloned version of the db item to avoid updating the in memory database through the two-way binding

--- www/js/controllers/edit.js
+++ www/js/controllers/edit.js
@@ -1,17 +1,19 @@
+/*global angular */
 define(['app'], function(app) {
   app.controller('EditCtrl', ['$scope', '$routeParams', '$navigate', 'database', 'inputDateHandler',
     function EditCtrl($scope, $routeParams, $navigate, database, inputDates) {
       $scope.mode = 'edit';
       $scope.title = 'Edit';
-      $scope.item = database.getItemById($routeParams.id);
+      // do deep copy because our own database is in-memory
+      $scope.item = angular.copy(database.getItemById($routeParams.id));
       $scope.item.dateValue = inputDates.format($scope.item.date);
-      
+
       $scope.save = function() {
         $scope.item.date = inputDates.parse($scope.item.dateValue);
-        
+
         var obj = database.editItem($scope.item.id, $scope.item);
         console.log("Updated", obj.id);
-        
+
         $navigate.go('/detail/' + obj.id, 'modal', true);
       };
     }
